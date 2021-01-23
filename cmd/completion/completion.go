ebe5014edfd00f1ab2c2dc8b6e5b1f9e6df06989
IdlePhysicist <idlephysicist@vivaldi.net>
2021-01-23 16:59:23 +0000

    Added more detailed help & usage information.

--- cmd/completion/completion.go
+++ cmd/completion/completion.go
@@ -24,9 +24,32 @@ import (
 
 func CreateCmd(rootCmd *cobra.Command) *cobra.Command {
 	c := &cobra.Command{
-		Use:   "completion",
-		Short: "output shell completion code",
-		Long: "Generate shell completion code for `bash` or `zsh` shells.",
+		Use:   "completion [bash|zsh]",
+		Short: "output shell completion code [bash|zsh]",
+		Long: `To load completions:
+
+Bash:
+
+$ source <(yourprogram completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ yourprogram completion bash > /etc/bash_completion.d/yourprogram
+MacOS:
+  $ yourprogram completion bash > /usr/local/etc/bash_completion.d/yourprogram
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ yourprogram completion zsh > "${fpath[1]}/_yourprogram"
+
+# You will need to start a new shell for this setup to take effect.
+`,
 
 		Args: cobra.ExactValidArgs(1),
 

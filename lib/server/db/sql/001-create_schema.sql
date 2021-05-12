43edad8c2ae1460cee5567aba9bbb767e8f500c2
sboehler <sboehler@users.noreply.github.com>
2021-05-13 00:35:51 +0200

    Versioning per table

--- lib/server/db/sql/001-create_schema.sql
+++ lib/server/db/sql/001-create_schema.sql
@@ -1,73 +1,76 @@
 CREATE TABLE versions (
-    version_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     description TEXT,
     created_at TEXT
 );
 
 CREATE TABLE changelists (
-    changelist_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     description TEXT,
     created_at TEXT
 );
 
 CREATE TABLE commodities(
-    commodity_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE account_ids (
+  id INTEGER PRIMARY KEY
 );
 
 CREATE TABLE accounts(
-    account_id INTEGER PRIMARY KEY,
+    account_id INTEGER NOT NULL REFERENCES account_ids,
     name TEXT NOT NULL,
-    type INTEGER NOT NULL
+    open_date TEXT NOT NULL,
+    close_date TEXT,
+    version_from INTEGER NOT NULL REFERENCES versions,
+    version_to INTEGER NOT NULL REFERENCES versions,
+    changelist_id INTEGER NOT NULL REFERENCES changelists,
+    PRIMARY KEY(account_id, version_from, version_to, changelist_id)
 );
-
-CREATE TABLE directives(
-    directive_id INTEGER PRIMARY KEY
-);
-
-CREATE TABLE directive_versions (
-  directive_id INTEGER NOT NULL REFERENCES directives,
-  directive_version_id INTEGER PRIMARY KEY,
-  version_from INTEGER NOT NULL REFERENCES versions,
-  version_to INTEGER REFERENCES versions,
-  changelist_id INTEGER REFERENCES changelists
-);
-
-CREATE INDEX directive_versions_directive_id_index on directive_versions(directive_id);
 
 CREATE TABLE prices (
-  directive_versions_id INTEGER NOT NULL REFERENCES directive_versions,
   date TEXT NOT NULL,
   commodity_id INTEGER NOT NULL REFERENCES commodities,
   target_commodity_id INTEGER NOT NULL REFERENCES commodities,
-  price DOUBLE NOT NULL
+  price DOUBLE NOT NULL,
+  version_from INTEGER NOT NULL REFERENCES versions,
+  version_to INTEGER NOT NULL REFERENCES versions,
+  changelist_id INTEGER NOT NULL REFERENCES changelists,
+  PRIMARY KEY(date, commodity_id, target_commodity_id, version_from, version_to, changelist_id)
 );
 
-CREATE INDEX prices_directive_versions_id_index on prices(directive_versions_id);
-CREATE INDEX prices_commodity_id_index on prices(commodity_id);
-CREATE INDEX prices_target_commodity_id_index on prices(target_commodity_id);
+CREATE TABLE assertion_ids (
+  id INTEGER PRIMARY KEY
+);
 
 CREATE TABLE assertions (
-  directive_versions_id INTEGER NOT NULL REFERENCES directive_versions,
+  assertion_id INTEGER NOT NULL REFERENCES assertion_ids,
   date TEXT NOT NULL,
   commodity_id INTEGER NOT NULL REFERENCES commodities,
   account_id INTEGER NOT NULL REFERENCES accounts,
-  amount TEXT NOT NULL
+  amount TEXT NOT NULL,
+  version_from INTEGER NOT NULL REFERENCES versions,
+  version_to INTEGER NOT NULL REFERENCES versions,
+  changelist_id INTEGER NOT NULL REFERENCES changelists,
+  PRIMARY KEY(assertion_id, version_from, version_to, changelist_id)
 );
 
-CREATE INDEX assertions_directive_versions_id_index on assertions(directive_versions_id);
-CREATE INDEX assertions_commodity_id_index on assertions(commodity_id);
-CREATE INDEX assertions_account_id_index on assertions(account_id);
-
+CREATE TABLE transaction_ids (
+  id INTEGER PRIMARY KEY
+);
 
 CREATE TABLE transactions (
-  transaction_id INTEGER PRIMARY KEY,
-  directive_versions_id INTEGER NOT NULL REFERENCES directive_versions,
+  id INTEGER PRIMARY KEY,
+  transaction_id INTEGER NOT NULL REFERENCES transaction_ids,
   date TEXT NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  version_from INTEGER NOT NULL REFERENCES versions,
+  version_to INTEGER NOT NULL REFERENCES versions,
+  changelist_id INTEGER NOT NULL REFERENCES changelists,
+  UNIQUE(transaction_id, version_from, version_to, changelist_id)
 );
-
-CREATE INDEX transactions_directive_versions_id_index on transactions(directive_versions_id);
 
 CREATE TABLE bookings (
   transaction_id INTEGER NOT NULL REFERENCES transactions,

eae494d83a192f57ab1c2a1e308bca590d6689ff
sboehler <sboehler@users.noreply.github.com>
2021-05-16 22:08:12 +0200

    Add accounts repo

--- lib/server/repo/accounts.go
+++ lib/server/repo/accounts.go
@@ -3,6 +3,7 @@ package repo
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/sboehler/knut/lib/server/model"
@@ -86,4 +87,72 @@ func ListAccounts(ctx context.Context, db db) ([]model.Account, error) {
 		return nil, rows.Err()
 	}
 	return res, nil
+}
+
+// UpdateAccount updates an account in the current version.
+func UpdateAccount(ctx context.Context, db db, id int, name string, openDate time.Time, closeDate *time.Time) (model.Account, error) {
+	var (
+		row *sql.Row
+		res = model.Account{}
+		err error
+	)
+	rslt, err := db.ExecContext(ctx, `
+	  UPDATE accounts 
+	  SET version_to = (SELECT MAX(version) FROM versions)
+	  WHERE account_id = ? AND version_to = 9223372036854775807`, id)
+	if err != nil {
+		return res, err
+	}
+	n, err := rslt.RowsAffected()
+	if err != nil {
+		return res, err
+	}
+	if n != 1 {
+		return res, fmt.Errorf("UpdateAccount() affected %d rows, expected 1", n)
+	}
+	row = db.QueryRowContext(ctx,
+		`INSERT INTO accounts(account_id, name, open_date, close_date, version_from, version_to)
+		SELECT ?, ?, ?, ?, (SELECT MAX(version) FROM versions), 9223372036854775807
+		ON CONFLICT (account_id, version_from, version_to) DO UPDATE
+		SET name = excluded.name, open_date = excluded.open_date, close_date = excluded.close_date
+		RETURNING name, datetime(open_date), datetime(close_date)`, res.ID, name, openDate, closeDate)
+	if row.Err() != nil {
+		return res, row.Err()
+	}
+	var (
+		os string
+		cs sql.NullString
+	)
+	if err = row.Scan(&res.Name, &os, &cs); err != nil {
+		return res, err
+	}
+	if err = parseDatetime(os, &res.OpenDate); err != nil {
+		return res, err
+	}
+	if cs.Valid {
+		var t time.Time
+		if err = parseDatetime(cs.String, &t); err != nil {
+			return res, err
+		}
+		res.CloseDate = &t
+	}
+	return res, nil
+}
+
+// DeleteAccount deletes an account in the current version.
+func DeleteAccount(ctx context.Context, db db, id int) (bool, error) {
+	res, err := db.ExecContext(ctx, `
+	  UPDATE accounts
+	  SET version_to = (SELECT MAX(version) from versions)
+	  WHERE accounts.account_id = ? 
+	  AND version_to > (SELECT MAX(version) from versions)
+	`, id)
+	if err != nil {
+		return false, err
+	}
+	rr, err := res.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	return rr == 1, nil
 }
--- lib/server/repo/accounts_test.go
+++ lib/server/repo/accounts_test.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/sboehler/knut/lib/server/model"
 )
 
@@ -30,7 +29,7 @@ func TestCreateAccount(t *testing.T) {
 	if err != nil {
 		t.Fatalf("CreateVersion() returned unexpected error: %v", err)
 	}
-	if diff := cmp.Diff(want, got, cmpopts.IgnoreFields(model.Account{}, "CreatedAt")); diff != "" {
+	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("CreateVersion() mismatch (-want +got):\n%s", diff)
 	}
 }
@@ -41,10 +40,9 @@ func TestListAccounts(t *testing.T) {
 		t2 = time.Date(2022, time.May, 14, 0, 0, 0, 0, time.UTC)
 	)
 	var (
-		ctx   = context.Background()
-		db    = createAndMigrateInMemoryDB(ctx, t)
-		descs = []string{"CCC", "BBB", "AAA"}
-		as    = []model.Account{
+		ctx = context.Background()
+		db  = createAndMigrateInMemoryDB(ctx, t)
+		as  = []model.Account{
 			{
 				Name:     "One",
 				OpenDate: t2,
@@ -56,11 +54,12 @@ func TestListAccounts(t *testing.T) {
 			},
 		}
 	)
-	_ = populateVersions(ctx, t, db, descs)
+	_ = populateVersions(ctx, t, db, []string{"v1"})
 	var want = populateAccounts(ctx, t, db, as)
 	sort.Slice(want, func(i, j int) bool {
 		return want[i].Name < want[j].Name
 	})
+	_ = populateVersions(ctx, t, db, []string{"v1"})
 
 	got, err := ListAccounts(ctx, db)
 
@@ -75,7 +74,78 @@ func TestListAccounts(t *testing.T) {
 	}
 }
 
+func TestUpdateAccounts(t *testing.T) {
+	var (
+		t1 = time.Date(2021, time.May, 14, 0, 0, 0, 0, time.UTC)
+		t2 = time.Date(2022, time.May, 14, 0, 0, 0, 0, time.UTC)
+	)
+	var (
+		ctx = context.Background()
+		db  = createAndMigrateInMemoryDB(ctx, t)
+		as  = []model.Account{
+			{
+				Name:     "One",
+				OpenDate: t2,
+			},
+		}
+	)
+	_ = populateVersions(ctx, t, db, []string{"v1"})
+	_ = populateAccounts(ctx, t, db, as)
+	_ = populateVersions(ctx, t, db, []string{"v2"})
+	var before = listAccounts(ctx, t, db)
+
+	got, err := UpdateAccount(ctx, db, before[0].ID, "Two", t1, &t2)
+
+	if err != nil {
+		t.Fatalf("UpdateAccount() returned unexpected error: %v", err)
+	}
+	var after = listAccounts(ctx, t, db)
+	if diff := cmp.Diff(after[0], got); diff != "" {
+		t.Errorf("ListAccounts() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestDeleteAccounts(t *testing.T) {
+	var (
+		t1 = time.Date(2021, time.May, 14, 0, 0, 0, 0, time.UTC)
+		t2 = time.Date(2022, time.May, 14, 0, 0, 0, 0, time.UTC)
+	)
+	var (
+		ctx = context.Background()
+		db  = createAndMigrateInMemoryDB(ctx, t)
+		as  = []model.Account{
+			{
+				Name:     "One",
+				OpenDate: t2,
+			},
+			{
+				Name:      "Two",
+				OpenDate:  t1,
+				CloseDate: &t2,
+			},
+		}
+	)
+	_ = populateVersions(ctx, t, db, []string{"v1"})
+	_ = populateAccounts(ctx, t, db, as)
+	_ = populateVersions(ctx, t, db, []string{"v1"})
+	var before = listAccounts(ctx, t, db)
+
+	ok, err := DeleteAccount(ctx, db, before[0].ID)
+
+	if err != nil {
+		t.Fatalf("DeleteAccount() returned unexpected error: %v", err)
+	}
+	if !ok {
+		t.Fatalf("DeleteAccount() did not delete an account, but expected it to do so")
+	}
+	var after = listAccounts(ctx, t, db)
+	if diff := cmp.Diff(before[1:], after); diff != "" {
+		t.Errorf("ListAccounts() mismatch (-want +got):\n%s", diff)
+	}
+}
+
 func populateAccounts(ctx context.Context, t *testing.T, db db, accounts []model.Account) []model.Account {
+	t.Helper()
 	var res []model.Account
 	for _, account := range accounts {
 		a, err := CreateAccount(ctx, db, account.Name, account.OpenDate, account.CloseDate)
@@ -83,6 +153,15 @@ func populateAccounts(ctx context.Context, t *testing.T, db db, accounts []model
 			t.Fatalf("CreateAccount() returned unexpected error: %v", err)
 		}
 		res = append(res, a)
+	}
+	return res
+}
+
+func listAccounts(ctx context.Context, t *testing.T, db db) []model.Account {
+	t.Helper()
+	res, err := ListAccounts(ctx, db)
+	if err != nil {
+		t.Fatalf("ListAccount() returned unexpected error: %v", err)
 	}
 	return res
 }

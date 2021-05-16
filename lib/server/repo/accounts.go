0737ac6c65c3fe8539ed368ffcbd2489fd74509f
sboehler <sboehler@users.noreply.github.com>
2021-05-16 23:03:07 +0200

    Deduplicate tests

--- lib/server/repo/accounts.go
+++ lib/server/repo/accounts.go
@@ -13,40 +13,24 @@ import (
 func CreateAccount(ctx context.Context, db db, name string, openDate time.Time, closeDate *time.Time) (model.Account, error) {
 	var (
 		row *sql.Row
-		res = model.Account{}
+		id  int
 		err error
 	)
-	if row = db.QueryRowContext(ctx, `INSERT INTO account_ids DEFAULT VALUES RETURNING id`); row.Err() != nil {
-		return res, row.Err()
+	row = db.QueryRowContext(ctx, `INSERT INTO account_ids DEFAULT VALUES RETURNING id`)
+	if row.Err() != nil {
+		return model.Account{}, row.Err()
 	}
-	if err = row.Scan(&res.ID); err != nil {
-		return res, err
+	if err = row.Scan(&id); err != nil {
+		return model.Account{}, err
 	}
 	row = db.QueryRowContext(ctx,
 		`INSERT INTO accounts(account_id, name, open_date, close_date, version_from, version_to)
 		SELECT ?, ?, ?, ?, (SELECT MAX(version) FROM versions), 9223372036854775807
-		RETURNING name, datetime(open_date), datetime(close_date)`, res.ID, name, openDate, closeDate)
+		RETURNING account_id, name, datetime(open_date), datetime(close_date)`, id, name, openDate, closeDate)
 	if row.Err() != nil {
-		return res, row.Err()
+		return model.Account{}, row.Err()
 	}
-	var (
-		os string
-		cs sql.NullString
-	)
-	if err = row.Scan(&res.Name, &os, &cs); err != nil {
-		return res, err
-	}
-	if err = parseDatetime(os, &res.OpenDate); err != nil {
-		return res, err
-	}
-	if cs.Valid {
-		var t time.Time
-		if err = parseDatetime(cs.String, &t); err != nil {
-			return res, err
-		}
-		res.CloseDate = &t
-	}
-	return res, nil
+	return rowToAccount(row)
 }
 
 // ListAccounts lists all accounts.
@@ -63,25 +47,11 @@ func ListAccounts(ctx context.Context, db db) ([]model.Account, error) {
 	}
 	defer rows.Close()
 	for rows.Next() {
-		var (
-			c  model.Account
-			os string
-			cs sql.NullString
-		)
-		if err := rows.Scan(&c.ID, &c.Name, &os, &cs); err != nil {
+		account, err := rowToAccount(rows)
+		if err != nil {
 			return nil, err
 		}
-		if err = parseDatetime(os, &c.OpenDate); err != nil {
-			return nil, err
-		}
-		if cs.Valid {
-			var t time.Time
-			if err = parseDatetime(cs.String, &t); err != nil {
-				return res, err
-			}
-			c.CloseDate = &t
-		}
-		res = append(res, c)
+		res = append(res, account)
 	}
 	if rows.Err() != nil && rows.Err() != sql.ErrNoRows {
 		return nil, rows.Err()
@@ -93,7 +63,6 @@ func ListAccounts(ctx context.Context, db db) ([]model.Account, error) {
 func UpdateAccount(ctx context.Context, db db, id int, name string, openDate time.Time, closeDate *time.Time) (model.Account, error) {
 	var (
 		row *sql.Row
-		res = model.Account{}
 		err error
 	)
 	rslt, err := db.ExecContext(ctx, `
@@ -101,51 +70,38 @@ func UpdateAccount(ctx context.Context, db db, id int, name string, openDate tim
 	  SET version_to = (SELECT MAX(version) FROM versions)
 	  WHERE account_id = ? AND version_to = 9223372036854775807`, id)
 	if err != nil {
-		return res, err
+		return model.Account{}, err
 	}
 	n, err := rslt.RowsAffected()
 	if err != nil {
-		return res, err
+		return model.Account{}, err
 	}
 	if n != 1 {
-		return res, fmt.Errorf("UpdateAccount() affected %d rows, expected 1", n)
+		return model.Account{}, fmt.Errorf("UpdateAccount() affected %d rows, expected 1", n)
 	}
 	row = db.QueryRowContext(ctx,
 		`INSERT INTO accounts(account_id, name, open_date, close_date, version_from, version_to)
 		SELECT ?, ?, ?, ?, (SELECT MAX(version) FROM versions), 9223372036854775807
 		ON CONFLICT (account_id, version_from, version_to) DO UPDATE
 		SET name = excluded.name, open_date = excluded.open_date, close_date = excluded.close_date
-		RETURNING name, datetime(open_date), datetime(close_date)`, res.ID, name, openDate, closeDate)
+		RETURNING account_id, name, datetime(open_date), datetime(close_date)`, id, name, openDate, closeDate)
 	if row.Err() != nil {
-		return res, row.Err()
+		return model.Account{}, row.Err()
 	}
-	var (
-		os string
-		cs sql.NullString
-	)
-	if err = row.Scan(&res.Name, &os, &cs); err != nil {
-		return res, err
-	}
-	if err = parseDatetime(os, &res.OpenDate); err != nil {
-		return res, err
-	}
-	if cs.Valid {
-		var t time.Time
-		if err = parseDatetime(cs.String, &t); err != nil {
-			return res, err
-		}
-		res.CloseDate = &t
-	}
-	return res, nil
+	return rowToAccount(row)
+}
+
+type scan interface {
+	Scan(dest ...interface{}) error
 }
 
 // DeleteAccount deletes an account in the current version.
 func DeleteAccount(ctx context.Context, db db, id int) (bool, error) {
 	res, err := db.ExecContext(ctx, `
-	  UPDATE accounts
-	  SET version_to = (SELECT MAX(version) from versions)
-	  WHERE accounts.account_id = ? 
-	  AND version_to > (SELECT MAX(version) from versions)
+	UPDATE accounts
+	SET version_to = (SELECT MAX(version) from versions)
+	WHERE accounts.account_id = ? 
+	AND version_to > (SELECT MAX(version) from versions)
 	`, id)
 	if err != nil {
 		return false, err
@@ -155,4 +111,27 @@ func DeleteAccount(ctx context.Context, db db, id int) (bool, error) {
 		return false, err
 	}
 	return rr == 1, nil
+}
+
+func rowToAccount(row scan) (model.Account, error) {
+	var (
+		res       model.Account
+		err       error
+		openDate  string
+		closeDate sql.NullString
+	)
+	if err = row.Scan(&res.ID, &res.Name, &openDate, &closeDate); err != nil {
+		return res, err
+	}
+	if err = parseDatetime(openDate, &res.OpenDate); err != nil {
+		return res, err
+	}
+	if closeDate.Valid {
+		var t time.Time
+		if err = parseDatetime(closeDate.String, &t); err != nil {
+			return res, err
+		}
+		res.CloseDate = &t
+	}
+	return res, nil
 }

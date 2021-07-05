e8abc0e06aa2769dd00313b4d892e1280dcd475f
sboehler <sboehler@users.noreply.github.com>
2021-07-05 21:42:38 +0200

    Fix prices tests

--- lib/server/database/idem/triggers.sql
+++ lib/server/database/idem/triggers.sql
@@ -1,5 +1,5 @@
 DROP VIEW IF EXISTS accounts;
-CREATE VIEW IF NOT EXISTS accounts AS 
+CREATE VIEW IF NOT EXISTS accounts AS
 SELECT id, name, open_date, close_date FROM accounts_history
 WHERE created_at <= CURRENT_TIMESTAMP AND deleted_at > CURRENT_TIMESTAMP;
 
@@ -14,9 +14,9 @@ CREATE TRIGGER IF NOT EXISTS accounts_update INSTEAD OF UPDATE ON accounts
 FOR EACH ROW
 BEGIN
  DELETE FROM accounts_history
-    WHERE id = new.id 
+    WHERE id = new.id
     AND created_at = CURRENT_TIMESTAMP;
-  UPDATE accounts_history SET deleted_at = CURRENT_TIMESTAMP 
+  UPDATE accounts_history SET deleted_at = CURRENT_TIMESTAMP
     WHERE id = new.id AND deleted_at = DATETIME('2999-12-31');
   INSERT INTO accounts_history(id, name, open_date, close_date)
     VALUES (new.id, new.name, new.open_date, new.close_date);
@@ -25,7 +25,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS accounts_delete INSTEAD OF DELETE ON accounts
 FOR EACH ROW
 BEGIN
-  UPDATE accounts_history SET deleted_at = CURRENT_TIMESTAMP 
+  UPDATE accounts_history SET deleted_at = CURRENT_TIMESTAMP
     WHERE id = old.id AND deleted_at = DATETIME('2999-12-31');
 END;
 
@@ -34,27 +34,28 @@ DROP VIEW IF EXISTS prices;
 CREATE VIEW IF NOT EXISTS prices AS
 SELECT date, commodity_id, target_commodity_id, price
 FROM prices_history
-WHERE created_at <= CURRENT_TIME AND deleted_at > CURRENT_TIMESTAMP;
+WHERE created_at <= CURRENT_TIMESTAMP AND deleted_at > CURRENT_TIMESTAMP;
 
 CREATE TRIGGER IF NOT EXISTS prices_insert INSTEAD OF INSERT ON prices
 FOR EACH ROW
 WHEN NOT EXISTS (
     SELECT 1
     FROM prices
-    WHERE date = new.date 
+    WHERE date = new.date
     AND commodity_id = new.commodity_id
-    AND target_commodity_id = new.target_commodity_id 
+    AND target_commodity_id = new.target_commodity_id
     AND price = new.price)
 BEGIN
   DELETE FROM prices_history
-    WHERE date = new.date 
+    WHERE date = new.date
     AND commodity_id = new.commodity_id
     AND target_commodity_id = new.target_commodity_id
     AND created_at = CURRENT_TIMESTAMP;
   UPDATE prices_history SET deleted_at = CURRENT_TIMESTAMP
-    WHERE date = new.date 
+    WHERE date = new.date
     AND commodity_id = new.commodity_id
-    AND target_commodity_id = new.target_commodity_id;
+    AND target_commodity_id = new.target_commodity_id
+    AND deleted_at = DATETIME('2999-12-31');
   INSERT INTO prices_history(date, commodity_id, target_commodity_id, price)
     VALUES(new.date, new.commodity_id, new.target_commodity_id, new.price);
 END;
@@ -62,9 +63,9 @@ END;
 CREATE TRIGGER IF NOT EXISTS prices_delete INSTEAD OF DELETE ON prices
 FOR EACH ROW
 BEGIN
-  UPDATE prices_history 
-    SET deleted_at = CURRENT_TIMESTAMP 
-    WHERE date = old.date 
+  UPDATE prices_history
+    SET deleted_at = CURRENT_TIMESTAMP
+    WHERE date = old.date
     AND commodity_id = old.commodity_id
     AND target_commodity_id = old.target_commodity_id
     AND deleted_at = DATETIME('2999-12-31');
@@ -72,7 +73,7 @@ END;
 
 
 DROP VIEW IF EXISTS transactions;
-CREATE VIEW IF NOT EXISTS transactions AS 
+CREATE VIEW IF NOT EXISTS transactions AS
 SELECT id, date, description FROM transactions_history
 WHERE created_at <= CURRENT_TIMESTAMP AND deleted_at > CURRENT_TIMESTAMP;
 
@@ -85,7 +86,7 @@ END;
 
 
 DROP VIEW IF EXISTS bookings;
-CREATE VIEW IF NOT EXISTS bookings AS 
+CREATE VIEW IF NOT EXISTS bookings AS
 SELECT id, amount, commodity_id, credit_account_id, debit_account_id FROM bookings_history
 WHERE created_at <= CURRENT_TIMESTAMP AND deleted_at > CURRENT_TIMESTAMP;
 

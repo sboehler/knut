ad5bcb40c5fbb5ad032f7419fb14e82f7ad1c963
sboehler <sboehler@users.noreply.github.com>
2021-08-01 13:14:58 +0200

    Test CreateCommodity using scenarios

--- lib/server/repo/commodities_test.go
+++ lib/server/repo/commodities_test.go
@@ -6,41 +6,55 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/sboehler/knut/lib/server/model"
 )
 
 func TestCreateCommodity(t *testing.T) {
 	var (
-		ctx     = context.Background()
-		db   db = createAndMigrateInMemoryDB(ctx, t)
-		name    = "CHF"
-		err  error
+		ctx      = context.Background()
+		db       = createAndMigrateInMemoryDB(ctx, t)
+		scenario = Save(ctx, t, db, Scenario{
+			Commodities: []model.Commodity{{Name: "CHF"}},
+		})
+		tests = []struct {
+			desc    string
+			name    string
+			wantErr error
+		}{
+			{
+				desc: "commodity does not exist",
+				name: "USD",
+			},
+			{
+				desc:    "existing commodity",
+				name:    "CHF",
+				wantErr: cmpopts.AnyError,
+			},
+		}
 	)
 
-	c, err := CreateCommodity(ctx, db, name)
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			db := beginTransaction(ctx, t, db)
+			defer db.Rollback()
 
-	if err != nil {
-		t.Errorf("Create(ctx, %s) returned error %v, expected none", name, err)
-	}
-	if c.Name != "CHF" {
-		t.Errorf("Create(ctx, %q) returned commodity with name %q, want %q", name, c.Name, name)
-	}
-	if c.ID <= 0 {
-		t.Errorf("Create(ctx, %s) returned commodity with ID %v, want a positive number", name, c.ID)
-	}
-}
-
-func TestCreateDuplicateCommodity(t *testing.T) {
-	var (
-		ctx  = context.Background()
-		db   = createAndMigrateInMemoryDB(ctx, t)
-		name = "AAA"
-	)
-	_ = populateCommodities(ctx, t, db, []string{name})
-
-	_, err := CreateCommodity(ctx, db, name)
-
-	if err == nil {
-		t.Errorf("Create(ctx, %s) returned no error, but expected one", name)
+			c, err := CreateCommodity(ctx, db, test.name)
+			if !cmp.Equal(test.wantErr, err, cmpopts.EquateErrors()) {
+				t.Fatalf("CreateCommodity returned error %v, expected %v", err, test.wantErr)
+				return
+			}
+			if test.wantErr != nil {
+				return
+			}
+			var (
+				want = Scenario{Commodities: append(scenario.Commodities, c)}
+				got  = Load(ctx, t, db)
+			)
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("CreateCommodityPrice() mismatch (-want +got):\n%s", diff)
+			}
+		})
 	}
 }
 

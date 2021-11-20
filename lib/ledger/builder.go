b45014d1f86de0a5c37d6256acf2ca03cd0c4a15
Silvio Böhler <sboehler@noreply.users.github.com>
2021-11-20 13:33:05 +0100

    Move filters to own file

--- lib/ledger/builder.go
+++ lib/ledger/builder.go
@@ -16,12 +16,8 @@ package ledger
 
 import (
 	"fmt"
-	"regexp"
 	"sort"
 	"time"
-
-	"github.com/sboehler/knut/lib/model/accounts"
-	"github.com/sboehler/knut/lib/model/commodities"
 )
 
 // FromDirectives reads directives from the given channel and
@@ -149,37 +145,4 @@ func (b *Builder) AddValue(a Value) {
 	}
 	var s = b.getOrCreate(a.Date)
 	s.Values = append(s.Values, a)
-}
-
-// Filter represents a filter creating a ledger.
-type Filter struct {
-	AccountsFilter, CommoditiesFilter *regexp.Regexp
-}
-
-// MatchAccount returns whether this filter matches the given Account.
-func (b Filter) MatchAccount(a *accounts.Account) bool {
-	return b.AccountsFilter == nil || b.AccountsFilter.MatchString(a.String())
-}
-
-// MatchCommodity returns whether this filter matches the given Commodity.
-func (b Filter) MatchCommodity(c *commodities.Commodity) bool {
-	return b.CommoditiesFilter == nil || b.CommoditiesFilter.MatchString(c.String())
-}
-
-// AccountFilter filters accounts.
-type AccountFilter struct {
-	Regex *regexp.Regexp
-}
-
-func (f *AccountFilter) match(a *accounts.Account) bool {
-	return f == nil || f.Regex.MatchString(a.String())
-}
-
-// CommodityFilter filters commodities.
-type CommodityFilter struct {
-	Regex *regexp.Regexp
-}
-
-func (f *CommodityFilter) match(a *commodities.Commodity) bool {
-	return f == nil || f.Regex.MatchString(a.String())
 }

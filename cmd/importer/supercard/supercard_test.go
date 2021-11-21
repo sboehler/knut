9c5e79774e41321ac62279b813cb3fc659055670
Silvio Böhler <sboehler@noreply.users.github.com>
2021-11-21 23:44:54 +0100

    Update license headers

--- cmd/importer/supercard/supercard_test.go
+++ cmd/importer/supercard/supercard_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Silvio Böhler
+// Copyright 2021 Silvio Böhler
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

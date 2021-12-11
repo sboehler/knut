8b91dff3a44b8fc1ad12248c4a38f36b11e32978
Silvio Böhler <sboehler@noreply.users.github.com>
2021-12-11 15:18:23 +0100

    Simplify asyncRunner

--- cmd/asyncbalance/asyncbalance.go
+++ cmd/asyncbalance/asyncbalance.go
@@ -18,7 +18,6 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"runtime/pprof"
@@ -27,6 +26,7 @@ import (
 
 	"github.com/sboehler/knut/cmd/flags"
 	"github.com/sboehler/knut/lib/balance"
+	"github.com/sboehler/knut/lib/date"
 	"github.com/sboehler/knut/lib/ledger"
 	"github.com/sboehler/knut/lib/parser"
 	"github.com/sboehler/knut/lib/report"
@@ -38,6 +38,8 @@ import (
 // CreateCmd creates the command.
 func CreateCmd() *cobra.Command {
 
+	var r runner
+
 	// Cmd is the balance command.
 	var c = &cobra.Command{
 		Use:   "asyncbalance",
@@ -48,118 +50,73 @@ func CreateCmd() *cobra.Command {
 
 		Hidden: true,
 
-		Run: run,
+		Run: r.run,
 	}
-	c.Flags().String("from", "", "from date")
-	c.Flags().String("cpuprofile", "", "file to write profile")
-	c.Flags().String("to", "", "to date")
-	c.Flags().IntP("last", "l", 0, "last n periods")
-	c.Flags().BoolP("diff", "d", false, "diff")
-	c.Flags().BoolP("show-commodities", "s", false, "Show commodities on their own rows")
-	c.Flags().Bool("days", false, "days")
-	c.Flags().Bool("weeks", false, "weeks")
-	c.Flags().Bool("months", false, "months")
-	c.Flags().Bool("quarters", false, "quarters")
-	c.Flags().Bool("years", false, "years")
-	c.Flags().StringP("val", "v", "", "valuate in the given commodity")
-	c.Flags().StringArrayP("collapse", "c", nil, "<level>,<regex>")
-	c.Flags().String("account", "", "filter accounts with a regex")
-	c.Flags().String("commodity", "", "filter commodities with a regex")
-	c.Flags().Int32("digits", 0, "round to number of digits")
-	c.Flags().BoolP("thousands", "k", false, "show numbers in units of 1000")
-	c.Flags().Bool("color", false, "print output in color")
+	r.setupFlags(c)
 	return c
 }
 
-func run(cmd *cobra.Command, args []string) {
-	if err := execute(cmd, args); err != nil {
-		fmt.Fprintln(cmd.ErrOrStderr(), err)
-		os.Exit(1)
-	}
+type runner struct {
+	cpuprofile                              string
+	from, to                                flags.DateFlag
+	last                                    int
+	diff, showCommodities, thousands, color bool
+	digits                                  int32
+	accounts, commodities                   flags.RegexFlag
+	period                                  flags.PeriodFlags
+	mapping                                 flags.MappingFlag
+	valuation                               flags.CommodityFlag
 }
 
-func execute(cmd *cobra.Command, args []string) error {
-	prof, err := cmd.Flags().GetString("cpuprofile")
-	if err != nil {
-		return err
-	}
-	if prof != "" {
-		f, err := os.Create(prof)
+func (r *runner) run(cmd *cobra.Command, args []string) {
+	if r.cpuprofile != "" {
+		f, err := os.Create(r.cpuprofile)
 		if err != nil {
 			log.Fatal(err)
 		}
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
-
-	var out = bufio.NewWriter(cmd.OutOrStdout())
-	defer out.Flush()
-	return process(cmd, args, out)
+	if err := r.execute(cmd, args); err != nil {
+		fmt.Fprintln(cmd.ErrOrStderr(), err)
+		os.Exit(1)
+	}
 }
 
-func process(cmd *cobra.Command, args []string, w io.Writer) error {
+func (r *runner) setupFlags(c *cobra.Command) {
+	c.Flags().StringVar(&r.cpuprofile, "cpuprofile", "", "file to write profile")
+	c.Flags().Var(&r.from, "from", "from date")
+	c.Flags().Var(&r.to, "to", "to date")
+	c.Flags().IntVar(&r.last, "last", 0, "last n periods")
+	c.Flags().BoolVarP(&r.diff, "diff", "d", false, "diff")
+	c.Flags().BoolVarP(&r.showCommodities, "show-commodities", "s", false, "Show commodities on their own rows")
+	r.period.Setup(c.Flags())
+	c.Flags().VarP(&r.valuation, "val", "v", "valuate in the given commodity")
+	c.Flags().VarP(&r.mapping, "map", "m", "<level>,<regex>")
+	c.Flags().Var(&r.accounts, "account", "filter accounts with a regex")
+	c.Flags().Var(&r.commodities, "commodity", "filter commodities with a regex")
+	c.Flags().Int32Var(&r.digits, "digits", 0, "round to number of digits")
+	c.Flags().BoolVarP(&r.thousands, "thousands", "k", false, "show numbers in units of 1000")
+	c.Flags().BoolVar(&r.color, "color", false, "print output in color")
+}
+
+func (r runner) execute(cmd *cobra.Command, args []string) error {
 	var (
-		ctx      = ledger.NewContext()
-		from, to time.Time
-		err      error
+		ctx = ledger.NewContext()
+
+		valuation *ledger.Commodity
+		period    date.Period
+
+		err error
 	)
-	if cmd.Flags().Changed("from") {
-		if from, err = flags.GetDateFlag(cmd, "from"); err != nil {
-			return err
-		}
-	}
-	if cmd.Flags().Changed("to") {
-		if to, err = flags.GetDateFlag(cmd, "to"); err != nil {
-			return err
-		}
-	} else {
+	if time.Time(r.to).IsZero() {
 		now := time.Now()
-		to = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+		r.to = flags.DateFlag(time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC))
 	}
-	last, err := cmd.Flags().GetInt("last")
-	if err != nil {
+	if valuation, err = r.valuation.Value(ctx); err != nil {
 		return err
 	}
-	var valuation *ledger.Commodity
-	if cmd.Flags().Changed("val") {
-		if valuation, err = flags.GetCommodityFlag(cmd, ctx, "val"); err != nil {
-			return err
-		}
-	}
-	showCommodities, err := cmd.Flags().GetBool("show-commodities")
-	if err != nil {
-		return err
-	}
-	diff, err := cmd.Flags().GetBool("diff")
-	if err != nil {
-		return err
-	}
-	digits, err := cmd.Flags().GetInt32("digits")
-	if err != nil {
-		return err
-	}
-	period, err := flags.GetPeriodFlag(cmd)
-	if err != nil {
-		return err
-	}
-	mapping, err := flags.GetCollapseFlag(cmd, "collapse")
-	if err != nil {
-		return err
-	}
-	filterAccounts, err := flags.GetRegexFlag(cmd, "account")
-	if err != nil {
-		return err
-	}
-	filterCommodities, err := flags.GetRegexFlag(cmd, "commodity")
-	if err != nil {
-		return err
-	}
-	thousands, err := cmd.Flags().GetBool("thousands")
-	if err != nil {
-		return err
-	}
-	color, err := cmd.Flags().GetBool("color")
-	if err != nil {
+	if period, err = r.period.Value(); err != nil {
 		return err
 	}
 
@@ -170,22 +127,22 @@ func process(cmd *cobra.Command, args []string, w io.Writer) error {
 		}
 		bal    = balance.New(ctx, valuation)
 		filter = ledger.Filter{
-			Accounts:    filterAccounts,
-			Commodities: filterCommodities,
+			Accounts:    r.accounts.Value(),
+			Commodities: r.commodities.Value(),
 		}
 		rep = &report.Report{
 			Value:   valuation != nil,
-			Mapping: mapping,
+			Mapping: r.mapping.Value(),
 		}
 		reportRenderer = report.Renderer{
 			Context:         ctx,
-			ShowCommodities: showCommodities || valuation == nil,
+			ShowCommodities: r.showCommodities || valuation == nil,
 			Report:          rep,
 		}
 		tableRenderer = table.TextRenderer{
-			Color:     color,
-			Thousands: thousands,
-			Round:     digits,
+			Color:     r.color,
+			Thousands: r.thousands,
+			Round:     r.digits,
 		}
 	)
 
@@ -204,11 +161,11 @@ func process(cmd *cobra.Command, args []string, w io.Writer) error {
 	b2 := balance.UpdatePrices(cotx, l, valuation, b1)
 	b3, err3Ch := balance.PostStage(cotx, l, b2)
 	snapshots, b4, err4Ch := balance.Snapshot(cotx, balance.SnapshotConfig{
-		Last:   last,
-		From:   from,
-		Diff:   diff,
+		Last:   r.last,
+		From:   r.from.Value(),
+		Diff:   r.diff,
 		Period: period,
-		To:     to,
+		To:     r.to.Value(),
 	}, l, b3)
 
 	var errcList = []<-chan error{err1Ch, err3Ch, err4Ch}
@@ -222,6 +179,9 @@ func process(cmd *cobra.Command, args []string, w io.Writer) error {
 	}()
 
 	errs := merge(errcList...)
+
+	var out = bufio.NewWriter(cmd.OutOrStdout())
+	defer out.Flush()
 
 	for {
 		select {
@@ -241,7 +201,7 @@ func process(cmd *cobra.Command, args []string, w io.Writer) error {
 			}
 		}
 		if snapshots == nil && errs == nil {
-			return tableRenderer.Render(reportRenderer.Render(), w)
+			return tableRenderer.Render(reportRenderer.Render(), out)
 		}
 
 	}

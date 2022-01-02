cb608d387ea62e0298646fc59be39c8b4ca32860
Silvio Böhler <sboehler@noreply.users.github.com>
2022-01-02 22:44:49 +0100

    Move report under val

--- cmd/balance/balance.go
+++ cmd/balance/balance.go
@@ -24,11 +24,11 @@ import (
 	"time"
 
 	"github.com/sboehler/knut/cmd/flags"
-	"github.com/sboehler/knut/lib/balance/report"
 	"github.com/sboehler/knut/lib/common/date"
 	"github.com/sboehler/knut/lib/common/table"
 	"github.com/sboehler/knut/lib/journal"
 	"github.com/sboehler/knut/lib/journal/process"
+	"github.com/sboehler/knut/lib/journal/val/report"
 
 	"github.com/spf13/cobra"
 )

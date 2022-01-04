8465865e7a7d27ec685de28110a4eb891d72a616
Silvio Böhler <sboehler@noreply.users.github.com>
2022-01-05 00:01:35 +0100

    Add more test data

--- lib/journal/process/astbuilder_test.go
+++ lib/journal/process/astbuilder_test.go
@@ -20,27 +20,46 @@ func (d DummyDirective) Position() ast.Range {
 }
 
 type TestData struct {
-	date1, date2 time.Time
-	open1        *ast.Open
-	price1       *ast.Price
+	date1, date2           time.Time
+	account1, account2     *journal.Account
+	commodity1, commodity2 *journal.Commodity
+	open1, open2           *ast.Open
+	price1                 *ast.Price
+	trx1                   *ast.Transaction
 }
 
 func newTestData(jctx journal.Context) TestData {
 	var (
-		date1  = date.Date(2022, 1, 4)
-		date2  = date.Date(2022, 1, 5)
-		price1 = &ast.Price{
+		date1      = date.Date(2022, 1, 4)
+		date2      = date.Date(2022, 1, 5)
+		account1   = jctx.Account("Assets:Account")
+		account2   = jctx.Account("Assets:Other")
+		commodity1 = jctx.Commodity("COM")
+		commodity2 = jctx.Commodity("TGT")
+		price1     = &ast.Price{
 			Date:      date1,
-			Commodity: jctx.Commodity("COM"),
-			Target:    jctx.Commodity("TGT"),
+			Commodity: commodity1,
+			Target:    commodity2,
 			Price:     decimal.NewFromInt(4),
 		}
-		open1 = &ast.Open{
-			Date:    date2,
-			Account: jctx.Account("Assets:Account"),
+		open1 = &ast.Open{Date: date2, Account: account1}
+		open2 = &ast.Open{Date: date2, Account: account2}
+		trx1  = &ast.Transaction{
+			Date:        date2,
+			Description: "foo",
+			Postings: []ast.Posting{
+				ast.NewPosting(account1, account2, commodity1, decimal.NewFromInt(10)),
+			},
 		}
 	)
-	return TestData{date1, date2, open1, price1}
+	return TestData{
+		date1, date2,
+		account1, account2,
+		commodity1, commodity2,
+		open1, open2,
+		price1,
+		trx1,
+	}
 }
 
 func TestASTBuilderHappyCase(t *testing.T) {

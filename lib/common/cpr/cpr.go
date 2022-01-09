0a5448ed66f91c80afe8451ef455b891e28da371
Silvio Böhler <sboehler@noreply.users.github.com>
2022-01-09 14:50:19 +0100

    Document the concurrency primitives

--- lib/common/cpr/cpr.go
+++ lib/common/cpr/cpr.go
@@ -3,6 +3,9 @@ package cpr
 
 import "context"
 
+// Get gets and returns a new T from the supplied channel. It returns
+// a T, a boolean which indicates whether the channel is still open, or\
+// an error from the supplied errCh.
 func Get[T any](ch <-chan T, errCh <-chan error) (T, bool, error) {
 	for {
 		select {
@@ -19,6 +22,9 @@ func Get[T any](ch <-chan T, errCh <-chan error) (T, bool, error) {
 	}
 }
 
+// Pop returns a new T from the ch. It returns a boolean which indicates
+// whether the channel is still open. The error indicates whether the context
+// has been canceled.
 func Pop[T any](ctx context.Context, ch <-chan T) (T, bool, error) {
 	var res T
 	select {
@@ -29,6 +35,8 @@ func Pop[T any](ctx context.Context, ch <-chan T) (T, bool, error) {
 	}
 }
 
+// Push tries to push a T to the ch. The error indicates whether the context
+// has been canceled.
 func Push[T any](ctx context.Context, ch chan<- T, ts ...T) error {
 	for _, t := range ts {
 		select {

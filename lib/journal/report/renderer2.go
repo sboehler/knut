6fdc179cd84154cc53412dc9e54634004baeb97c
Silvio Böhler <sboehler@noreply.users.github.com>
2022-06-06 01:20:26 +0200

    WIP renderer improvements

--- lib/journal/report/renderer2.go
+++ lib/journal/report/renderer2.go
@@ -21,7 +21,6 @@ import (
 	"github.com/sboehler/knut/lib/common/amounts2"
 	"github.com/sboehler/knut/lib/common/table"
 	"github.com/sboehler/knut/lib/journal"
-	"github.com/shopspring/decimal"
 )
 
 // Renderer2 renders a report.
@@ -82,17 +81,17 @@ func (rn *Renderer2) Render(r amounts2.Amounts) *table.Table {
 	sides := []struct {
 		neg    bool
 		accs   []*journal.Account
-		totals map[time.Time]decimal.Decimal
+		totals amounts2.Amounts
 	}{
 		{
 			neg:    false,
 			accs:   al,
-			totals: make(map[time.Time]decimal.Decimal),
+			totals: make(amounts2.Amounts),
 		},
 		{
 			neg:    true,
 			accs:   eie,
-			totals: make(map[time.Time]decimal.Decimal),
+			totals: make(amounts2.Amounts),
 		},
 	}
 	for _, side := range sides {
@@ -101,23 +100,25 @@ func (rn *Renderer2) Render(r amounts2.Amounts) *table.Table {
 				rn.table.AddEmptyRow()
 			}
 			row := rn.table.AddRow().AddIndented(a.Segment(), 2*(a.Level()-1))
-			line := make(map[time.Time]decimal.Decimal)
+			line := make(amounts2.Amounts)
 			for _, d := range rn.dates {
 				for c := range rn.Context.Commodities().Enumerate() {
 					k := amounts2.Key{Date: d, Account: a, Commodity: c, Valuation: rn.Valuation}
 					if v, ok := rn.amounts[k]; ok {
-						line[d] = line[d].Add(v)
+						dk := amounts2.Key{Date: d}
+						line[dk] = line[dk].Add(v)
 					}
 				}
 			}
 			for _, d := range rn.dates {
-				if v, ok := line[d]; ok && !v.IsZero() {
+				k := amounts2.Key{Date: d}
+				if v, ok := line[k]; ok && !v.IsZero() {
 					if side.neg {
 						row.AddNumber(v.Neg())
-						side.totals[d] = side.totals[d].Add(v.Neg())
+						side.totals[k] = side.totals[k].Add(v.Neg())
 					} else {
 						row.AddNumber(v)
-						side.totals[d] = side.totals[d].Add(v)
+						side.totals[k] = side.totals[k].Add(v)
 					}
 				} else {
 					row.AddEmpty()
@@ -127,13 +128,13 @@ func (rn *Renderer2) Render(r amounts2.Amounts) *table.Table {
 		rn.table.AddEmptyRow()
 		row := rn.table.AddRow().AddText("Total", table.Left)
 		for _, d := range rn.dates {
-			row.AddNumber(side.totals[d])
+			row.AddNumber(side.totals[amounts2.Key{Date: d}])
 		}
 		rn.table.AddSeparatorRow()
 	}
 	row := rn.table.AddRow().AddText("Delta", table.Left)
 	for _, d := range rn.dates {
-		row.AddNumber(sides[0].totals[d].Sub(sides[1].totals[d]))
+		row.AddNumber(sides[0].totals[amounts2.Key{Date: d}].Sub(sides[1].totals[amounts2.Key{Date: d}]))
 	}
 	rn.table.AddSeparatorRow()
 	return rn.table
@@ -147,4 +148,71 @@ func (rn Renderer2) subtree(as map[*journal.Account]struct{}) map[*journal.Accou
 		}
 	}
 	return m
+}
+
+type extractor struct {
+	Amounts       amounts2.Amounts
+	SortCommodity *journal.Commodity
+
+	dates                   []time.Time
+	accounts                []*journal.Account
+	commodities, valuations []*journal.Commodity
+}
+
+func (ext *extractor) Init(jctx journal.Context) {
+	dates := make(map[time.Time]struct{})
+	leafs := make(map[*journal.Account]struct{})
+	valuations := make(map[*journal.Commodity]struct{})
+	commodities := make(map[*journal.Commodity]struct{})
+
+	accountWeights := make(map[*journal.Account]float64)
+
+	for k, v := range ext.Amounts {
+		dates[k.Date] = struct{}{}
+		leafs[k.Account] = struct{}{}
+		valuations[k.Valuation] = struct{}{}
+		commodities[k.Commodity] = struct{}{}
+		if k.Valuation != nil && k.Valuation == ext.SortCommodity {
+			f, _ := v.Abs().Float64()
+			accountWeights[k.Account] += f
+		} else {
+			// default: sort alphabetically
+			accountWeights[k.Account] = 0
+		}
+	}
+
+	// sort dates
+	for d := range dates {
+		ext.dates = append(ext.dates, d)
+	}
+	sort.Slice(ext.dates, func(i, j int) bool { return ext.dates[i].Before(ext.dates[j]) })
+
+	// fill parents and sort accounts
+	accounts := make(map[*journal.Account]struct{})
+	for a := range leafs {
+		accounts[a] = struct{}{}
+		for p := a.Parent(); p != nil; p = p.Parent() {
+			accounts[p] = struct{}{}
+			accountWeights[p] += accountWeights[a]
+		}
+	}
+	for _, a := range jctx.Accounts().SortedPreOrder(accountWeights) {
+		if _, ok := accounts[a]; ok {
+			ext.accounts = append(ext.accounts, a)
+		}
+	}
+
+	for a := range commodities {
+		ext.commodities = append(ext.commodities, a)
+	}
+	sort.Slice(ext.commodities, func(i, j int) bool { return ext.commodities[i].String() < ext.commodities[j].String() })
+
+	for a := range valuations {
+		ext.valuations = append(ext.valuations, a)
+	}
+	sort.Slice(ext.valuations, func(i, j int) bool {
+		// valuation can be nil
+		v0, v1 := ext.valuations[i], ext.valuations[j]
+		return v1 != nil && (v0 == nil || ext.valuations[i].String() < ext.valuations[j].String())
+	})
 }

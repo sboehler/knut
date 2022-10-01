5f1608f88fd5f51dab74c380b2fb43be2abe29f4
Silvio Böhler <sboehler@noreply.users.github.com>
2022-10-02 00:09:14 +0200

    Add generated files to repo

--- server/proto/proto.go
+++
@@ -1,3 +0,0 @@
-//go:generate protoc --go_opt=paths=source_relative --go_out=. --go-grpc_out=. --go-grpc_opt=paths=source_relative service.proto  --grpc-web_out=import_style=typescript,mode=grpcweb:../../web/src/proto --js_out=import_style=commonjs,binary:../../web/src/proto
-
-package service_go_proto
---
+++ web/src/proto/service_pb.d.ts
@@ -0,0 +1,40 @@
+import * as jspb from 'google-protobuf'
+
+
+
+export class HelloRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): HelloRequest;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): HelloRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: HelloRequest): HelloRequest.AsObject;
+  static serializeBinaryToWriter(message: HelloRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): HelloRequest;
+  static deserializeBinaryFromReader(message: HelloRequest, reader: jspb.BinaryReader): HelloRequest;
+}
+
+export namespace HelloRequest {
+  export type AsObject = {
+    name: string,
+  }
+}
+
+export class HelloResponse extends jspb.Message {
+  getGreeting(): string;
+  setGreeting(value: string): HelloResponse;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): HelloResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: HelloResponse): HelloResponse.AsObject;
+  static serializeBinaryToWriter(message: HelloResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): HelloResponse;
+  static deserializeBinaryFromReader(message: HelloResponse, reader: jspb.BinaryReader): HelloResponse;
+}
+
+export namespace HelloResponse {
+  export type AsObject = {
+    greeting: string,
+  }
+}
+

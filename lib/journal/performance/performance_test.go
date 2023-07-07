755bee98ec25478ec8ccff76e816ce1c98e508ae
Silvio Böhler <sboehler@noreply.users.github.com>
2023-07-07 21:24:49 +0200

    Align code style

--- lib/journal/performance/performance_test.go
+++ lib/journal/performance/performance_test.go
@@ -3,301 +3,298 @@ package performance
 import (
 	"regexp"
 	"testing"
-	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/sboehler/knut/lib/common/date"
 	"github.com/sboehler/knut/lib/common/filter"
 	"github.com/sboehler/knut/lib/journal"
 	"github.com/shopspring/decimal"
 )
 
 func TestComputeFlows(t *testing.T) {
-	var (
-		ctx          = journal.NewContext()
-		chf, _       = ctx.GetCommodity("CHF")
-		usd, _       = ctx.GetCommodity("USD")
-		gbp, _       = ctx.GetCommodity("GBP")
-		aapl, _      = ctx.GetCommodity("AAPL")
-		portfolio, _ = ctx.GetAccount("Assets:Portfolio")
-		acc1, _      = ctx.GetAccount("Assets:Acc1")
-		acc2, _      = ctx.GetAccount("Assets:Acc2")
-		dividend, _  = ctx.GetAccount("Income:Dividends")
-		expense, _   = ctx.GetAccount("Expenses:Investments")
-		equity, _    = ctx.GetAccount("Equity:Equity")
-	)
+
+	ctx := journal.NewContext()
+	chf := ctx.Commodity("CHF")
+	usd := ctx.Commodity("USD")
+	gbp := ctx.Commodity("GBP")
+	aapl := ctx.Commodity("AAPL")
+	portfolio := ctx.Account("Assets:Portfolio")
+	acc1 := ctx.Account("Assets:Acc1")
+	acc2 := ctx.Account("Assets:Acc2")
+	dividend := ctx.Account("Income:Dividends")
+	expense := ctx.Account("Expenses:Investments")
+	equity := ctx.Account("Equity:Equity")
+
 	chf.IsCurrency = true
 	usd.IsCurrency = true
 	gbp.IsCurrency = true
 
-	var (
-		tests = []struct {
-			desc string
-			trx  *journal.Transaction
-			want *journal.Performance
-		}{
-			{
-				desc: "outflow",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilder{
+	tests := []struct {
+		desc string
+		trx  *journal.Transaction
+		want *journal.Performance
+	}{
+		{
+			desc: "outflow",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilder{
+					Credit:    portfolio,
+					Debit:     acc2,
+					Amount:    decimal.NewFromInt(1),
+					Commodity: usd,
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{Outflow: pcv{usd: -1.0}},
+		},
+		{
+			desc: "inflow",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilder{
+					Credit:    acc1,
+					Debit:     portfolio,
+					Amount:    decimal.NewFromInt(1),
+					Commodity: usd,
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{Inflow: pcv{usd: 1.0}},
+		},
+		{
+			desc: "dividend",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilder{
+					Credit:    dividend,
+					Debit:     portfolio,
+					Amount:    decimal.NewFromInt(1),
+					Commodity: usd,
+					Targets:   []*journal.Commodity{aapl, usd},
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{
+				InternalInflow:  pcv{usd: 1.0},
+				InternalOutflow: pcv{aapl: -1.0},
+			},
+		},
+		{
+			desc: "expense",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilder{
+					Credit:    portfolio,
+					Debit:     expense,
+					Amount:    decimal.NewFromInt(1),
+					Commodity: usd,
+					Targets:   []*journal.Commodity{aapl, usd},
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{
+				InternalInflow:  pcv{aapl: 1.0},
+				InternalOutflow: pcv{usd: -1.0},
+			},
+		},
+		{
+			desc: "expense with effect on porfolio",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilder{
+					Credit:    portfolio,
+					Debit:     expense,
+					Amount:    decimal.NewFromInt(1),
+					Commodity: usd,
+					Targets:   make([]*journal.Commodity, 0),
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{
+				InternalOutflow: pcv{usd: -1.0},
+				PortfolioInflow: 1.0,
+			},
+		},
+		{
+			desc: "stock purchase",
+			trx: journal.TransactionBuilder{
+
+				Postings: journal.PostingBuilders{
+					{
 						Credit:    portfolio,
-						Debit:     acc2,
-						Amount:    decimal.NewFromInt(1),
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(1010),
 						Commodity: usd,
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{Outflow: pcv{usd: -1.0}},
-			},
-			{
-				desc: "inflow",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilder{
-						Credit:    acc1,
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
+					{
+						Credit:    equity,
 						Debit:     portfolio,
-						Amount:    decimal.NewFromInt(1),
-						Commodity: usd,
-					}.Build(),
+						Amount:    decimal.NewFromInt(1000),
+						Commodity: aapl,
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
 				}.Build(),
-				want: &journal.Performance{Inflow: pcv{usd: 1.0}},
+			}.Build(),
+			want: &journal.Performance{
+				InternalInflow:  pcv{aapl: 1010.0},
+				InternalOutflow: pcv{usd: -1010.0},
 			},
-			{
-				desc: "dividend",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilder{
-						Credit:    dividend,
+		},
+		{
+			desc: "stock purchase with fee",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilders{
+					{
+						Credit:    portfolio,
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(1010),
+						Commodity: usd,
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
+					{
+						Credit:    equity,
 						Debit:     portfolio,
-						Amount:    decimal.NewFromInt(1),
+						Amount:    decimal.NewFromInt(1000),
+						Commodity: aapl,
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
+					{
+						Credit:    portfolio,
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(10),
 						Commodity: usd,
-						Targets:   []*journal.Commodity{aapl, usd},
-					}.Build(),
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
 				}.Build(),
-				want: &journal.Performance{
-					InternalInflow:  pcv{usd: 1.0},
-					InternalOutflow: pcv{aapl: -1.0},
-				},
+			}.Build(),
+			want: &journal.Performance{
+				InternalInflow:  pcv{aapl: 1020.0},
+				InternalOutflow: pcv{usd: -1020.0},
 			},
-			{
-				desc: "expense",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilder{
+		},
+		{
+			desc: "stock sale",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilders{
+					{
+						Credit:    portfolio,
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(1000),
+						Commodity: aapl,
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
+					{
+						Credit:    equity,
+						Debit:     portfolio,
+						Amount:    decimal.NewFromInt(990),
+						Commodity: usd,
+						Targets:   []*journal.Commodity{usd, aapl},
+					},
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{
+				InternalInflow:  pcv{usd: 990.0},
+				InternalOutflow: pcv{aapl: -990.0},
+			},
+		},
+
+		{
+			desc: "forex without fee",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilders{
+					{
+						Credit:    portfolio,
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(1400),
+						Commodity: gbp,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
+					{
+						Credit:    equity,
+						Debit:     portfolio,
+						Amount:    decimal.NewFromInt(1350),
+						Commodity: usd,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
+				}.Build(),
+			}.Build(),
+			want: &journal.Performance{
+				InternalOutflow: pcv{gbp: -1375.0},
+				InternalInflow:  pcv{usd: 1375.0},
+			},
+		},
+		{
+			desc: "forex with fee",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilders{
+					{
+						Credit:    portfolio,
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(1400),
+						Commodity: gbp,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
+					{
+						Credit:    equity,
+						Debit:     portfolio,
+						Amount:    decimal.NewFromInt(1350),
+						Commodity: usd,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
+					{
 						Credit:    portfolio,
 						Debit:     expense,
-						Amount:    decimal.NewFromInt(1),
-						Commodity: usd,
-						Targets:   []*journal.Commodity{aapl, usd},
-					}.Build(),
+						Amount:    decimal.NewFromInt(10),
+						Commodity: chf,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
 				}.Build(),
-				want: &journal.Performance{
-					InternalInflow:  pcv{aapl: 1.0},
-					InternalOutflow: pcv{usd: -1.0},
-				},
+			}.Build(),
+			want: &journal.Performance{
+				InternalOutflow: pcv{gbp: -1370.0, chf: -10},
+				InternalInflow:  pcv{usd: 1380.0},
 			},
-			{
-				desc: "expense with effect on porfolio",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilder{
+		},
+		{
+			desc: "forex with native fee",
+			trx: journal.TransactionBuilder{
+				Postings: journal.PostingBuilders{
+					{
+						Credit:    portfolio,
+						Debit:     equity,
+						Amount:    decimal.NewFromInt(1400),
+						Commodity: gbp,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
+					{
+						Credit:    equity,
+						Debit:     portfolio,
+						Amount:    decimal.NewFromInt(1350),
+						Commodity: usd,
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
+					{
 						Credit:    portfolio,
 						Debit:     expense,
-						Amount:    decimal.NewFromInt(1),
+						Amount:    decimal.NewFromInt(10),
 						Commodity: usd,
-						Targets:   make([]*journal.Commodity, 0),
-					}.Build(),
+						Targets:   []*journal.Commodity{usd, gbp},
+					},
 				}.Build(),
-				want: &journal.Performance{
-					InternalOutflow: pcv{usd: -1.0},
-					PortfolioInflow: 1.0,
-				},
+			}.Build(),
+			want: &journal.Performance{
+				InternalOutflow: pcv{gbp: -1370.0},
+				InternalInflow:  pcv{usd: 1370.0},
 			},
-			{
-				desc: "stock purchase",
-				trx: journal.TransactionBuilder{
+		},
+	}
 
-					Postings: journal.PostingBuilders{
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(1010),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-						{
-							Credit:    equity,
-							Debit:     portfolio,
-							Amount:    decimal.NewFromInt(1000),
-							Commodity: aapl,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{
-					InternalInflow:  pcv{aapl: 1010.0},
-					InternalOutflow: pcv{usd: -1010.0},
-				},
-			},
-			{
-				desc: "stock purchase with fee",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilders{
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(1010),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-						{
-							Credit:    equity,
-							Debit:     portfolio,
-							Amount:    decimal.NewFromInt(1000),
-							Commodity: aapl,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(10),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{
-					InternalInflow:  pcv{aapl: 1020.0},
-					InternalOutflow: pcv{usd: -1020.0},
-				},
-			},
-			{
-				desc: "stock sale",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilders{
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(1000),
-							Commodity: aapl,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-						{
-							Credit:    equity,
-							Debit:     portfolio,
-							Amount:    decimal.NewFromInt(990),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, aapl},
-						},
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{
-					InternalInflow:  pcv{usd: 990.0},
-					InternalOutflow: pcv{aapl: -990.0},
-				},
-			},
-
-			{
-				desc: "forex without fee",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilders{
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(1400),
-							Commodity: gbp,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-						{
-							Credit:    equity,
-							Debit:     portfolio,
-							Amount:    decimal.NewFromInt(1350),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{
-					InternalOutflow: pcv{gbp: -1375.0},
-					InternalInflow:  pcv{usd: 1375.0},
-				},
-			},
-			{
-				desc: "forex with fee",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilders{
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(1400),
-							Commodity: gbp,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-						{
-							Credit:    equity,
-							Debit:     portfolio,
-							Amount:    decimal.NewFromInt(1350),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-						{
-							Credit:    portfolio,
-							Debit:     expense,
-							Amount:    decimal.NewFromInt(10),
-							Commodity: chf,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{
-					InternalOutflow: pcv{gbp: -1370.0, chf: -10},
-					InternalInflow:  pcv{usd: 1380.0},
-				},
-			},
-			{
-				desc: "forex with native fee",
-				trx: journal.TransactionBuilder{
-					Postings: journal.PostingBuilders{
-						{
-							Credit:    portfolio,
-							Debit:     equity,
-							Amount:    decimal.NewFromInt(1400),
-							Commodity: gbp,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-						{
-							Credit:    equity,
-							Debit:     portfolio,
-							Amount:    decimal.NewFromInt(1350),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-						{
-							Credit:    portfolio,
-							Debit:     expense,
-							Amount:    decimal.NewFromInt(10),
-							Commodity: usd,
-							Targets:   []*journal.Commodity{usd, gbp},
-						},
-					}.Build(),
-				}.Build(),
-				want: &journal.Performance{
-					InternalOutflow: pcv{gbp: -1370.0},
-					InternalInflow:  pcv{usd: 1370.0},
-				},
-			},
-		}
-	)
 	for _, test := range tests {
 		t.Run(test.desc, func(t *testing.T) {
-			var (
-				d = &journal.Day{
-					Date:         time.Date(2021, 11, 15, 0, 0, 0, 0, time.UTC),
-					Transactions: []*journal.Transaction{test.trx},
-				}
+			day := &journal.Day{
+				Date:         date.Date(2021, 11, 15),
+				Transactions: []*journal.Transaction{test.trx},
+			}
+			calc := Calculator{
+				AccountFilter: filter.ByName[*journal.Account]([]*regexp.Regexp{
+					regexp.MustCompile("Assets:Portfolio"),
+				}),
+				Valuation: chf,
+			}
 
-				fc = Calculator{
-					AccountFilter: filter.ByName[*journal.Account]([]*regexp.Regexp{
-						regexp.MustCompile("Assets:Portfolio")}),
-					Valuation: chf,
-				}
-			)
-
-			got := fc.computeFlows(d)
+			got := calc.computeFlows(day)
 
 			if diff := cmp.Diff(test.want, got); diff != "" {
 				t.Fatalf("unexpected diff (-want, +got):\n%s", diff)

df5446f13e597a6aacfb0d8cd045ad88fd9dfae4
Silvio Böhler <sboehler@noreply.users.github.com>
2023-07-22 23:30:44 +0200

    Unify tests

--- lib/syntax/parser/parser_test.go
+++ lib/syntax/parser/parser_test.go
@@ -4,294 +4,303 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/sboehler/knut/lib/syntax"
 )
 
-func TestParseCommodity(t *testing.T) {
-	for _, test := range []struct {
-		text    string
-		want    syntax.Commodity
-		wantErr bool
-	}{
-		{
-			text: "foobar",
-			want: syntax.Commodity{Start: 0, End: 6},
-		},
-		{
-			text:    "",
-			want:    syntax.Commodity{Start: 0, End: 0},
-			wantErr: true,
-		},
-		{
-			text:    "(foobar)",
-			want:    syntax.Commodity{Start: 0, End: 0},
-			wantErr: true,
-		},
-	} {
+type testcase[T any] struct {
+	text    string
+	want    func(string) T
+	wantErr bool
+}
+
+type parserTest[T any] struct {
+	tests []testcase[T]
+	desc  string
+	fn    func(p *Parser) (T, error)
+}
+
+func (tests parserTest[T]) run(t *testing.T) {
+	t.Helper()
+	for _, test := range tests.tests {
 		t.Run(test.text, func(t *testing.T) {
-			p := setupParser(t, test.text)
+			parser := New(test.text, "")
+			if err := parser.Advance(); err != nil {
+				t.Fatalf("s.Advance() = %v, want nil", err)
+			}
 
-			got, err := p.parseCommodity()
+			got, err := tests.fn(parser)
 
-			if (err != nil) != test.wantErr || !cmp.Equal(got, test.want, cmpopts.IgnoreFields(syntax.Commodity{}, "Text")) {
-				t.Fatalf("p.parseCommodity() = %#v, %#v, want %#v, error presence %t", got, err, test.want, test.wantErr)
+			if (err != nil) != test.wantErr {
+				t.Fatalf("%s returned error %v, want error presence %t", tests.desc, err, test.wantErr)
+			}
+			if diff := cmp.Diff(test.want(test.text), got); diff != "" {
+				t.Fatalf("%s returned unexpected diff (-want/+got)\n%s\n", tests.desc, diff)
 			}
 		})
 	}
+}
+
+func TestParseCommodity(t *testing.T) {
+	parserTest[syntax.Commodity]{
+		tests: []testcase[syntax.Commodity]{
+			{
+				text: "foobar",
+				want: func(s string) syntax.Commodity {
+					return syntax.Commodity{Start: 0, End: 6, Text: s}
+				},
+			},
+			{
+				text: "",
+				want: func(s string) syntax.Commodity {
+					return syntax.Commodity{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "(foobar)",
+				want: func(s string) syntax.Commodity {
+					return syntax.Commodity{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
+		},
+		fn: func(p *Parser) (syntax.Commodity, error) {
+			return p.parseCommodity()
+		},
+		desc: "p.parseCommodity()",
+	}.run(t)
 }
 
 func TestParseAccount(t *testing.T) {
-	for _, test := range []struct {
-		text    string
-		want    syntax.Account
-		wantErr bool
-	}{
-		{
-			text: "foobar",
-			want: syntax.Account{Start: 0, End: 6},
+	parserTest[syntax.Account]{
+		tests: []testcase[syntax.Account]{
+			{
+				text: "foobar",
+				want: func(s string) syntax.Account {
+					return syntax.Account{Start: 0, End: 6, Text: s}
+				},
+			},
+			{
+				text: "",
+				want: func(s string) syntax.Account {
+					return syntax.Account{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "(foobar)",
+				want: func(s string) syntax.Account {
+					return syntax.Account{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "ABC:",
+				want: func(s string) syntax.Account {
+					return syntax.Account{Start: 0, End: 4, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "ABC:B",
+				want: func(s string) syntax.Account {
+					return syntax.Account{Start: 0, End: 5, Text: s}
+				},
+			},
+			{
+				text: "ABC:B:C:D",
+				want: func(s string) syntax.Account {
+					return syntax.Account{Start: 0, End: 9, Text: s}
+				},
+			},
 		},
-		{
-			text:    "",
-			want:    syntax.Account{Start: 0, End: 0},
-			wantErr: true,
+		desc: "p.parseAccount()",
+		fn: func(p *Parser) (syntax.Account, error) {
+			return p.parseAccount()
 		},
-		{
-			text:    "(foobar)",
-			want:    syntax.Account{Start: 0, End: 0},
-			wantErr: true,
-		},
-		{
-			text:    "ABC:",
-			want:    syntax.Account{Start: 0, End: 4},
-			wantErr: true,
-		},
-		{
-			text: "ABC:B",
-			want: syntax.Account{Start: 0, End: 5},
-		},
-		{
-			text: "ABC:B:C:D",
-			want: syntax.Account{Start: 0, End: 9},
-		},
-	} {
-		t.Run(test.text, func(t *testing.T) {
-			p := setupParser(t, test.text)
-
-			got, err := p.parseAccount()
-
-			if (err != nil) != test.wantErr || !cmp.Equal(got, test.want, cmpopts.IgnoreFields(syntax.Account{}, "Text")) {
-				t.Fatalf("p.parseAccount() = %#v, %#v, want %#v, error presence %t", got, err, test.want, test.wantErr)
-			}
-		})
-	}
+	}.run(t)
 }
 
 func TestParseAccountMacro(t *testing.T) {
-	for _, test := range []struct {
-		text    string
-		want    syntax.AccountMacro
-		wantErr bool
-	}{
-		{
-			text: "$foobar",
-			want: syntax.AccountMacro{Start: 0, End: 7},
+	parserTest[syntax.AccountMacro]{
+		tests: []testcase[syntax.AccountMacro]{
+			{
+				text: "$foobar",
+				want: func(s string) syntax.AccountMacro {
+					return syntax.AccountMacro{Start: 0, End: 7, Text: s}
+				},
+			},
+			{
+				text: "$foo1",
+				want: func(s string) syntax.AccountMacro {
+					return syntax.AccountMacro{Start: 0, End: 4, Text: s}
+				},
+			},
+			{
+				text: "$1foo",
+				want: func(s string) syntax.AccountMacro {
+					return syntax.AccountMacro{Start: 0, End: 1, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "",
+				want: func(s string) syntax.AccountMacro {
+					return syntax.AccountMacro{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "foobar",
+				want: func(s string) syntax.AccountMacro {
+					return syntax.AccountMacro{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
 		},
-		{
-			text: "$foo1",
-			want: syntax.AccountMacro{Start: 0, End: 4},
+		desc: "p.parseAccountMacro()",
+		fn: func(p *Parser) (syntax.AccountMacro, error) {
+			return p.parseAccountMacro()
 		},
-		{
-			text:    "$1foo",
-			want:    syntax.AccountMacro{Start: 0, End: 1},
-			wantErr: true,
-		},
-		{
-			text:    "",
-			want:    syntax.AccountMacro{Start: 0, End: 0},
-			wantErr: true,
-		},
-		{
-			text:    "foobar",
-			want:    syntax.AccountMacro{Start: 0, End: 0},
-			wantErr: true,
-		},
-	} {
-		t.Run(test.text, func(t *testing.T) {
-			p := setupParser(t, test.text)
-
-			got, err := p.parseAccountMacro()
-
-			if (err != nil) != test.wantErr || !cmp.Equal(got, test.want, cmpopts.IgnoreFields(syntax.AccountMacro{}, "Text")) {
-				t.Fatalf("p.parseAccountMacro() = %#v, %#v, want %#v, error presence %t", got, err, test.want, test.wantErr)
-			}
-		})
-	}
+	}.run(t)
 }
 
 func TestParseDecimal(t *testing.T) {
-	for _, test := range []struct {
-		text    string
-		want    syntax.Decimal
-		wantErr bool
-	}{
-		{
-			text: "10",
-			want: syntax.Decimal{Start: 0, End: 2},
+	parserTest[syntax.Decimal]{
+		tests: []testcase[syntax.Decimal]{
+			{
+				text: "10",
+				want: func(s string) syntax.Decimal {
+					return syntax.Decimal{Start: 0, End: 2, Text: s}
+				},
+			},
+			{
+				text: "-10",
+				want: func(s string) syntax.Decimal {
+					return syntax.Decimal{Start: 0, End: 3, Text: s}
+				},
+			},
+			{
+				text: "-10.0",
+				want: func(s string) syntax.Decimal {
+					return syntax.Decimal{Start: 0, End: 5, Text: s}
+				},
+			},
+			{
+				text: "-10.",
+				want: func(s string) syntax.Decimal {
+					return syntax.Decimal{Start: 0, End: 4, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "99.",
+				want: func(s string) syntax.Decimal {
+					return syntax.Decimal{Start: 0, End: 3, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "foo",
+				want: func(s string) syntax.Decimal {
+					return syntax.Decimal{Start: 0, End: 0, Text: s}
+				},
+				wantErr: true,
+			},
 		},
-		{
-			text: "-10",
-			want: syntax.Decimal{Start: 0, End: 3},
+		desc: "p.parseDecimal()",
+		fn: func(p *Parser) (syntax.Decimal, error) {
+			return p.parseDecimal()
 		},
-		{
-			text: "-10.0",
-			want: syntax.Decimal{Start: 0, End: 5},
-		},
-		{
-			text:    "-10.",
-			want:    syntax.Decimal{Start: 0, End: 4},
-			wantErr: true,
-		},
-		{
-			text:    "99.",
-			want:    syntax.Decimal{Start: 0, End: 3},
-			wantErr: true,
-		},
-		{
-			text:    "foo",
-			want:    syntax.Decimal{Start: 0, End: 0},
-			wantErr: true,
-		},
-	} {
-		t.Run(test.text, func(t *testing.T) {
-			p := setupParser(t, test.text)
-
-			got, err := p.parseDecimal()
-
-			if (err != nil) != test.wantErr || !cmp.Equal(got, test.want, cmpopts.IgnoreFields(syntax.Decimal{}, "Text")) {
-				t.Fatalf("p.parseDecimal() = %#v, %#v, want %#v, error presence %t", got, err, test.want, test.wantErr)
-			}
-		})
-	}
+	}.run(t)
 }
 
 func TestParseDate(t *testing.T) {
-	for _, test := range []struct {
-		text    string
-		want    func(string) syntax.Date
-		wantErr bool
-	}{
-		{
-			text: "2023-05-31",
-			want: func(s string) syntax.Date {
-				return syntax.Date{Start: 0, End: 10, Text: s}
+	parserTest[syntax.Date]{
+		tests: []testcase[syntax.Date]{
+			{
+				text: "2023-05-31",
+				want: func(s string) syntax.Date {
+					return syntax.Date{Start: 0, End: 10, Text: s}
+				},
+			},
+			{
+				text: "202-05-31",
+				want: func(s string) syntax.Date {
+					return syntax.Date{Start: 0, End: 3, Text: s}
+				},
+				wantErr: true,
+			},
+			{
+				text: "20205-31",
+				want: func(s string) syntax.Date {
+					return syntax.Date{Start: 0, End: 4, Text: s}
+				},
+				wantErr: true,
 			},
 		},
-		{
-			text: "202-05-31",
-			want: func(s string) syntax.Date {
-				return syntax.Date{Start: 0, End: 3, Text: s}
-			},
-			wantErr: true,
+		desc: "p.parseDate()",
+		fn: func(p *Parser) (syntax.Date, error) {
+			return p.parseDate()
 		},
-		{
-			text: "20205-31",
-			want: func(s string) syntax.Date {
-				return syntax.Date{Start: 0, End: 4, Text: s}
-			},
-			wantErr: true,
-		},
-	} {
-		t.Run(test.text, func(t *testing.T) {
-			p := setupParser(t, test.text)
-
-			got, err := p.parseDate()
-
-			if (err != nil) != test.wantErr {
-				t.Fatalf("p.parseDate() returned error %v, want error presence %t", err, test.wantErr)
-			}
-			if diff := cmp.Diff(test.want(test.text), got); diff != "" {
-				t.Fatalf("p.parseDate() returned unexpected diff (-want/+got)\n%s\n", diff)
-			}
-		})
-	}
+	}.run(t)
 }
 
 func TestParseBooking(t *testing.T) {
-	for _, test := range []struct {
-		text    string
-		want    func(string) syntax.Booking
-		wantErr bool
-	}{
-		{
-			text: "A:B C:D 100.0 CHF",
-			want: func(t string) syntax.Booking {
-				return syntax.Booking{
-					Pos:       syntax.Pos{Start: 0, End: 17, Text: t},
-					Credit:    syntax.Account{Start: 0, End: 3, Text: t},
-					Debit:     syntax.Account{Start: 4, End: 7, Text: t},
-					Amount:    syntax.Decimal{Start: 8, End: 13, Text: t},
-					Commodity: syntax.Commodity{Start: 14, End: 17, Text: t},
-				}
+	parserTest[syntax.Booking]{
+		tests: []testcase[syntax.Booking]{
+			{
+				text: "A:B C:D 100.0 CHF",
+				want: func(t string) syntax.Booking {
+					return syntax.Booking{
+						Pos:       syntax.Pos{Start: 0, End: 17, Text: t},
+						Credit:    syntax.Account{Start: 0, End: 3, Text: t},
+						Debit:     syntax.Account{Start: 4, End: 7, Text: t},
+						Amount:    syntax.Decimal{Start: 8, End: 13, Text: t},
+						Commodity: syntax.Commodity{Start: 14, End: 17, Text: t},
+					}
+				},
+			},
+			{
+				text: "$dividend C:D 100.0 CHF",
+				want: func(t string) syntax.Booking {
+					return syntax.Booking{
+						Pos:         syntax.Pos{Start: 0, End: 23, Text: t},
+						CreditMacro: syntax.AccountMacro{Start: 0, End: 9, Text: t},
+						Debit:       syntax.Account{Start: 10, End: 13, Text: t},
+						Amount:      syntax.Decimal{Start: 14, End: 19, Text: t},
+						Commodity:   syntax.Commodity{Start: 20, End: 23, Text: t},
+					}
+				},
+			},
+			{
+				text: "A:B C:D 100.0",
+				want: func(t string) syntax.Booking {
+					return syntax.Booking{
+						Pos:    syntax.Pos{Start: 0, End: 13, Text: t},
+						Credit: syntax.Account{Start: 0, End: 3, Text: t},
+						Debit:  syntax.Account{Start: 4, End: 7, Text: t},
+						Amount: syntax.Decimal{Start: 8, End: 13, Text: t},
+					}
+				},
+				wantErr: true,
+			},
+			{
+				text: "C:D  $dividend  100.0  CHF",
+				want: func(t string) syntax.Booking {
+					return syntax.Booking{
+						Pos:        syntax.Pos{Start: 0, End: 26, Text: t},
+						Credit:     syntax.Account{Start: 0, End: 3, Text: t},
+						DebitMacro: syntax.AccountMacro{Start: 5, End: 14, Text: t},
+						Amount:     syntax.Decimal{Start: 16, End: 21, Text: t},
+						Commodity:  syntax.Commodity{Start: 23, End: 26, Text: t},
+					}
+				},
 			},
 		},
-		{
-			text: "$dividend C:D 100.0 CHF",
-			want: func(t string) syntax.Booking {
-				return syntax.Booking{
-					Pos:         syntax.Pos{Start: 0, End: 23, Text: t},
-					CreditMacro: syntax.AccountMacro{Start: 0, End: 9, Text: t},
-					Debit:       syntax.Account{Start: 10, End: 13, Text: t},
-					Amount:      syntax.Decimal{Start: 14, End: 19, Text: t},
-					Commodity:   syntax.Commodity{Start: 20, End: 23, Text: t},
-				}
-			},
+		desc: "p.parseBooking()",
+		fn: func(p *Parser) (syntax.Booking, error) {
+			return p.parseBooking()
 		},
-		{
-			text: "A:B C:D 100.0",
-			want: func(t string) syntax.Booking {
-				return syntax.Booking{
-					Pos:    syntax.Pos{Start: 0, End: 13, Text: t},
-					Credit: syntax.Account{Start: 0, End: 3, Text: t},
-					Debit:  syntax.Account{Start: 4, End: 7, Text: t},
-					Amount: syntax.Decimal{Start: 8, End: 13, Text: t},
-				}
-			},
-			wantErr: true,
-		},
-		{
-			text: "C:D  $dividend  100.0  CHF",
-			want: func(t string) syntax.Booking {
-				return syntax.Booking{
-					Pos:        syntax.Pos{Start: 0, End: 26, Text: t},
-					Credit:     syntax.Account{Start: 0, End: 3, Text: t},
-					DebitMacro: syntax.AccountMacro{Start: 5, End: 14, Text: t},
-					Amount:     syntax.Decimal{Start: 16, End: 21, Text: t},
-					Commodity:  syntax.Commodity{Start: 23, End: 26, Text: t},
-				}
-			},
-		},
-	} {
-		t.Run(test.text, func(t *testing.T) {
-			p := setupParser(t, test.text)
-
-			got, err := p.parseBooking()
-
-			if (err != nil) != test.wantErr {
-				t.Fatalf("p.parseBooking() returned error %v, want error presence %t", err, test.wantErr)
-			}
-			if diff := cmp.Diff(test.want(test.text), got); diff != "" {
-				t.Fatalf("p.parseBooking() returned unexpected diff (-want/+got)\n%s\n", diff)
-			}
-		})
-	}
-}
-
-func setupParser(t *testing.T, text string) *Parser {
-	t.Helper()
-	parser := New(text, "")
-	if err := parser.Advance(); err != nil {
-		t.Fatalf("s.Advance() = %v, want nil", err)
-	}
-	return parser
+	}.run(t)
 }

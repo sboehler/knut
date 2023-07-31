9bc4669da968deddc305376c62e2136a3366ecad
Silvio Böhler <sboehler@noreply.users.github.com>
2023-07-31 22:26:53 +0200

    Use new bayes lib

--- cmd/infer/infer.go
+++ cmd/infer/infer.go
@@ -19,17 +19,16 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"os"
 
 	"github.com/natefinch/atomic"
 	"github.com/spf13/cobra"
 
-	"github.com/sboehler/knut/cmd/flags"
 	"github.com/sboehler/knut/lib/common/cpr"
-	"github.com/sboehler/knut/lib/journal"
-	"github.com/sboehler/knut/lib/journal/bayes"
-	"github.com/sboehler/knut/lib/journal/format"
+	"github.com/sboehler/knut/lib/syntax"
+	"github.com/sboehler/knut/lib/syntax/bayes"
+	"github.com/sboehler/knut/lib/syntax/parser"
+	"github.com/sboehler/knut/lib/syntax/printer"
 )
 
 // CreateCmd creates the command.
@@ -48,13 +47,13 @@ func CreateCmd() *cobra.Command {
 }
 
 type runner struct {
-	account      flags.AccountFlag
+	account      string
 	trainingFile string
 	inplace      bool
 }
 
 func (r *runner) setupFlags(cmd *cobra.Command) {
-	cmd.Flags().VarP(&r.account, "account", "a", "account name")
+	cmd.Flags().StringVarP(&r.account, "account", "a", "Expenses:TBD", "account name")
 	cmd.Flags().BoolVarP(&r.inplace, "inplace", "i", false, "infer the accounts inplace")
 	cmd.Flags().StringVarP(&r.trainingFile, "training-file", "t", "", "the journal file with existing data")
 	cmd.MarkFlagRequired("training-file")
@@ -69,84 +68,61 @@ func (r *runner) run(cmd *cobra.Command, args []string) {
 
 func (r *runner) execute(cmd *cobra.Command, args []string) (errors error) {
 	var (
-		jctx       = journal.NewContext()
 		targetFile = args[0]
-		account    *journal.Account
 		err        error
 	)
-	if account, err = r.account.ValueWithDefault(jctx, jctx.Account("Expenses:TBD")); err != nil {
-		return err
-	}
-	model, err := train(cmd.Context(), jctx, r.trainingFile, account)
+	model, err := train(cmd.Context(), r.trainingFile, r.account)
 	if err != nil {
 		return err
 	}
-	directives, err := r.parseAndInfer(cmd.Context(), jctx, model, targetFile)
+	file, err := r.parseAndInfer(cmd.Context(), model, targetFile)
 	if err != nil {
 		return err
 	}
+	var p printer.Printer
 	if r.inplace {
 		var buf bytes.Buffer
-		if err := r.writeTo(directives, targetFile, &buf); err != nil {
+		if err := p.Format(file, &buf); err != nil {
 			return err
 		}
 		return atomic.WriteFile(targetFile, &buf)
 	} else {
 		out := bufio.NewWriter(cmd.OutOrStdout())
-		if err := r.writeTo(directives, targetFile, out); err != nil {
-			return err
-		}
-		return out.Flush()
+		defer out.Flush()
+		return p.Format(file, out)
 	}
 }
 
-func train(ctx context.Context, jctx journal.Context, file string, exclude *journal.Account) (*bayes.Model, error) {
-	var (
-		j = journal.RecursiveParser{Context: jctx, File: file}
-		m = bayes.NewModel(exclude)
-	)
-	err := cpr.Consume(ctx, j.Parse(ctx), func(d any) error {
+func train(ctx context.Context, file string, account string) (*bayes.Model, error) {
+	model := bayes.NewModel(account)
+	return model, cpr.Consume(ctx, parser.Parse(ctx, file), func(d any) error {
 		switch t := d.(type) {
 		case error:
 			return t
-		case *journal.Transaction:
-			m.Update(t)
+		case syntax.Transaction:
+			model.Update(&t)
 		}
 		return nil
 	})
-	return m, err
 }
 
-func (r *runner) parseAndInfer(ctx context.Context, jctx journal.Context, model *bayes.Model, targetFile string) ([]journal.Directive, error) {
-	p, cls, err := journal.ParserFromPath(jctx, targetFile)
+func (r *runner) parseAndInfer(ctx context.Context, model *bayes.Model, targetFile string) (syntax.File, error) {
+	text, err := os.ReadFile(targetFile)
 	if err != nil {
-		return nil, err
+		return syntax.File{}, err
 	}
-	defer cls()
-	var directives []journal.Directive
-	for {
-		d, err := p.Next()
-		if err == io.EOF {
-			return directives, nil
-		}
-		if err != nil {
-			return nil, err
-		}
-		switch t := d.(type) {
-		case *journal.Transaction:
-			model.Infer(t)
-			directives = append(directives, t)
-		default:
-			directives = append(directives, d)
-		}
+	p := parser.New(string(text), targetFile)
+	if err := p.Advance(); err != nil {
+		return syntax.File{}, err
 	}
-}
-
-func (r *runner) writeTo(directives []journal.Directive, targetFile string, out io.Writer) error {
-	srcFile, err := os.Open(targetFile)
+	f, err := p.ParseFile()
 	if err != nil {
-		return err
+		return syntax.File{}, err
 	}
-	defer srcFile.Close()
-	return format.Format(directives, bufio.NewReader(srcFile), out)
+	for i := range f.Directives {
+		if t, ok := f.Directives[i].Directive.(syntax.Transaction); ok {
+			model.Infer(&t)
+		}
+	}
+	return f, nil
 }

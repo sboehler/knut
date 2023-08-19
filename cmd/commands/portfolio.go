1e407df45019a029d06542c1a9474db11dbcae27
Silvio Böhler <sboehler@noreply.users.github.com>
2023-08-19 15:17:00 +0200

    Reorganize portfolio commands

--- cmd/commands/portfolio.go
+++ cmd/commands/portfolio.go
@@ -15,99 +15,18 @@
 package commands
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"runtime/pprof"
-
 	"github.com/spf13/cobra"
 
-	"github.com/sboehler/knut/cmd/flags"
-	"github.com/sboehler/knut/lib/common/date"
-	"github.com/sboehler/knut/lib/common/filter"
-	"github.com/sboehler/knut/lib/journal"
-	"github.com/sboehler/knut/lib/journal/performance"
-	"github.com/sboehler/knut/lib/model"
-	"github.com/sboehler/knut/lib/model/registry"
+	returns "github.com/sboehler/knut/cmd/commands/portfolio"
 )
 
 // CreatePortfolioCommand creates the command.
 func CreatePortfolioCommand() *cobra.Command {
-
-	var r portfolioRunner
-	// Cmd is the balance command.
 	c := &cobra.Command{
 		Use:   "portfolio",
-		Short: "compute portfolio returns",
-		Long:  `Compute portfolio returns.`,
-
-		Args: cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
-
-		Hidden: true,
-
-		Run: r.run,
+		Short: "Portfolio management commands",
+		Long:  `Portfolio management commands`,
 	}
-	r.setupFlags(c)
+	c.AddCommand(returns.CreateReturnsCommand())
 	return c
-}
-
-type portfolioRunner struct {
-	cpuprofile            string
-	valuation             flags.CommodityFlag
-	accounts, commodities flags.RegexFlag
-	period                flags.PeriodFlag
-	interval              flags.IntervalFlags
-}
-
-func (r *portfolioRunner) setupFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&r.cpuprofile, "cpuprofile", "", "file to write profile")
-	cmd.Flags().VarP(&r.valuation, "val", "v", "valuate in the given commodity")
-	cmd.Flags().Var(&r.accounts, "account", "filter accounts with a regex")
-	cmd.Flags().Var(&r.commodities, "commodity", "filter commodities with a regex")
-	r.period.Setup(cmd, date.Period{End: date.Today()})
-	r.interval.Setup(cmd, date.Once)
-
-}
-
-func (r *portfolioRunner) run(cmd *cobra.Command, args []string) {
-	if r.cpuprofile != "" {
-		f, err := os.Create(r.cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	}
-	if err := r.execute(cmd, args); err != nil {
-		fmt.Fprintln(cmd.ErrOrStderr(), err)
-		os.Exit(1)
-	}
-}
-
-func (r *portfolioRunner) execute(cmd *cobra.Command, args []string) error {
-	ctx := cmd.Context()
-	reg := registry.New()
-	valuation, err := r.valuation.Value(reg)
-	if err != nil {
-		return err
-	}
-	j, err := journal.FromPath(ctx, reg, args[0])
-	if err != nil {
-		return err
-	}
-	partition := date.NewPartition(r.period.Value().Clip(j.Period()), r.interval.Value(), 0)
-	calculator := &performance.Calculator{
-		Context:         reg,
-		Valuation:       valuation,
-		AccountFilter:   filter.ByName[*model.Account](r.accounts.Regex()),
-		CommodityFilter: filter.ByName[*model.Commodity](r.commodities.Regex()),
-	}
-	_, err = j.Process(
-		journal.ComputePrices(valuation),
-		journal.Balance(reg, valuation),
-		calculator.ComputeValues(),
-		calculator.ComputeFlows(),
-		performance.Perf(j, partition),
-	)
-	return err
 }

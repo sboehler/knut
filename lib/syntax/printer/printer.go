f6bd9f004babb99e50734fa38016df78c66ea5eb
Silvio Böhler <sboehler@noreply.users.github.com>
2024-02-11 13:37:00 +0100

    Simplify count handling

--- lib/syntax/printer/printer.go
+++ lib/syntax/printer/printer.go
@@ -42,7 +42,12 @@ func (p *Printer) Write(bs []byte) (int, error) {
 }
 
 // PrintDirective prints a directive to the given Writer.
-func (p *Printer) PrintDirective(directive directives.Directive) (n int, err error) {
+func (p *Printer) PrintDirective(directive directives.Directive) (int, error) {
+	err := p.printDirective(directive)
+	return p.count, err
+}
+
+func (p *Printer) printDirective(directive directives.Directive) error {
 	switch d := directive.Directive.(type) {
 	case directives.Transaction:
 		return p.printTransaction(d)
@@ -57,14 +62,13 @@ func (p *Printer) PrintDirective(directive directives.Directive) (n int, err err
 	case directives.Price:
 		return p.printPrice(d)
 	}
-	return 0, fmt.Errorf("unknown directive: %v", directive)
+	return fmt.Errorf("unknown directive: %v", directive)
 }
 
-func (p *Printer) printTransaction(t directives.Transaction) (n int, err error) {
-	start := p.count
+func (p *Printer) printTransaction(t directives.Transaction) error {
 	if !t.Addons.Accrual.Empty() {
-		if _, err := p.printAccrual(t.Addons.Accrual); err != nil {
-			return p.count - start, err
+		if err := p.printAccrual(t.Addons.Accrual); err != nil {
+			return err
 		}
 	}
 	if !t.Addons.Performance.Empty() {
@@ -73,68 +77,74 @@ func (p *Printer) printTransaction(t directives.Transaction) (n int, err error) 
 			s = append(s, t.Extract())
 		}
 		if _, err := fmt.Fprintf(p, "@performance(%s)\n", strings.Join(s, ",")); err != nil {
-			return p.count - start, err
+			return err
 		}
 	}
 	if _, err := fmt.Fprintf(p, `%s "%s"`, t.Date.Extract(), t.Description.Content.Extract()); err != nil {
-		return p.count - start, err
+		return err
 	}
-	if _, err = io.WriteString(p, "\n"); err != nil {
-		return p.count - start, err
+	if _, err := io.WriteString(p, "\n"); err != nil {
+		return err
 	}
 	for _, po := range t.Bookings {
-		if _, err := p.printPosting(po); err != nil {
-			return p.count - start, err
+		if err := p.printPosting(po); err != nil {
+			return err
 		}
-		if _, err = io.WriteString(p, "\n"); err != nil {
-			return p.count - start, err
+		if _, err := io.WriteString(p, "\n"); err != nil {
+			return err
 		}
 	}
-	return n, nil
+	return nil
 }
 
-func (p *Printer) printAccrual(a directives.Accrual) (n int, err error) {
-	return fmt.Fprintf(p, "@accrue %s %s %s %s\n", a.Interval.Extract(), a.Start.Extract(), a.End.Extract(), a.Account.Extract())
+func (p *Printer) printAccrual(a directives.Accrual) error {
+	_, err := fmt.Fprintf(p, "@accrue %s %s %s %s\n", a.Interval.Extract(), a.Start.Extract(), a.End.Extract(), a.Account.Extract())
+	return err
 }
 
-func (p *Printer) printPosting(t directives.Booking) (int, error) {
-	return fmt.Fprintf(p, "%-*s %-*s %10s %s", p.padding, t.Credit.Extract(), p.padding, t.Debit.Extract(), t.Quantity.Extract(), t.Commodity.Extract())
+func (p *Printer) printPosting(t directives.Booking) error {
+	_, err := fmt.Fprintf(p, "%-*s %-*s %10s %s", p.padding, t.Credit.Extract(), p.padding, t.Debit.Extract(), t.Quantity.Extract(), t.Commodity.Extract())
+	return err
 }
 
-func (p *Printer) printOpen(o directives.Open) (int, error) {
-	return fmt.Fprintf(p, "%s open %s", o.Date.Extract(), o.Account.Extract())
+func (p *Printer) printOpen(o directives.Open) error {
+	_, err := fmt.Fprintf(p, "%s open %s", o.Date.Extract(), o.Account.Extract())
+	return err
 }
 
-func (p *Printer) printClose(c directives.Close) (int, error) {
-	return fmt.Fprintf(p, "%s close %s", c.Date.Extract(), c.Account.Extract())
+func (p *Printer) printClose(c directives.Close) error {
+	_, err := fmt.Fprintf(p, "%s close %s", c.Date.Extract(), c.Account.Extract())
+	return err
 }
 
-func (p *Printer) printPrice(pr directives.Price) (int, error) {
-	return fmt.Fprintf(p, "%s price %s %s %s", pr.Date.Extract(), pr.Commodity.Extract(), pr.Price.Extract(), pr.Target.Extract())
+func (p *Printer) printPrice(pr directives.Price) error {
+	_, err := fmt.Fprintf(p, "%s price %s %s %s", pr.Date.Extract(), pr.Commodity.Extract(), pr.Price.Extract(), pr.Target.Extract())
+	return err
 }
 
-func (p *Printer) printInclude(i directives.Include) (int, error) {
-	return fmt.Fprintf(p, "include \"%s\"", i.IncludePath.Content.Extract())
+func (p *Printer) printInclude(i directives.Include) error {
+	_, err := fmt.Fprintf(p, "include \"%s\"", i.IncludePath.Content.Extract())
+	return err
 }
 
-func (p *Printer) printAssertion(a directives.Assertion) (int, error) {
-	start := p.count
+func (p *Printer) printAssertion(a directives.Assertion) error {
 	if _, err := fmt.Fprintf(p, "%s balance", a.Date.Extract()); err != nil {
-		return p.count - start, err
+		return err
 	}
 	if len(a.Balances) == 1 {
-		return fmt.Fprintf(p, " %s %s %s", a.Balances[0].Account.Extract(), a.Balances[0].Quantity.Extract(), a.Balances[0].Commodity.Extract())
+		_, err := fmt.Fprintf(p, " %s %s %s", a.Balances[0].Account.Extract(), a.Balances[0].Quantity.Extract(), a.Balances[0].Commodity.Extract())
+		return err
 	} else {
 		if _, err := io.WriteString(p, "\n"); err != nil {
-			return p.count - start, err
+			return err
 		}
 		for _, bal := range a.Balances {
 			if _, err := fmt.Fprintf(p, "%s %s %s\n", bal.Account.Extract(), bal.Quantity.Extract(), bal.Commodity.Extract()); err != nil {
-				return p.count - start, err
+				return err
 			}
 		}
 	}
-	return p.count - start, nil
+	return nil
 }
 
 func (p *Printer) PrintFile(f directives.File) (int, error) {
